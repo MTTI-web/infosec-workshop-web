e2573b8e68489e772cea866064641c10d92573af
MTTI-web <srijankumardps@gmail.com>
2026-08-19 16:42:23 +0530

    added link

--- server/lib/db.js
+++ server/lib/db.js
@@ -29,7 +29,7 @@ async function getDb() {
         INSERT INTO users (username, password, is_hidden, secret_pin, post) VALUES
           ('bob', 'letmein123', 0, '4821', 'Hello world! This is my first post.'),
           ('alice', 'sunshine99', 0, '7734', 'Sunshine makes everything better!'),
-          ('admin', 'S3cr3t-Adm1n-Pass!', 1, '9999', 'After passing Aurora East Street, you notice a strange inscription carved into the wall. It says b11f9fa4e239b9c8fc708e5e9b470e25215e046201acf5cb22e1d4843b37c22a');
+          ('admin', 'S3cr3t-Adm1n-Pass!', 1, '9999', 'After passing Aurora East Street, you notice a strange inscription carved into the wall. It says b11f9fa4e239b9c8fc708e5e9b470e25215e046201acf5cb22e1d4843b37c22a. Btw this is my site: https://ai-jail-break-demo-phi.vercel.app/');
       `);
 
       return db;
